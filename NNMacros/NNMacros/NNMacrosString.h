29ee870f7082c2299367d9af03ddd57775db3994
GuHaijun <243297288@qq.com>
2019-04-26 22:31:25 +0800

    #fix invalid suffix on literal; C++11 requires a space between literal and identifier

--- NNMacros/NNMacros/NNMacrosString.h
+++ NNMacros/NNMacros/NNMacrosString.h
@@ -19,14 +19,14 @@
 metamacro_concat(NN_stringConnect, metamacro_argcount(__VA_ARGS__))(str, __VA_ARGS__)
 
 //#define NN_stringConnect1(str0, str1)      (str0 str1)
-#define NN_stringConnect1(str0, str1)      @""str0""str1""
-#define NN_stringConnect2(str0, str1, str2)      @""str0""str1""str2""
-#define NN_stringConnect3(str0, str1, str2, str3)      @""str0""str1""str2""str3""
-#define NN_stringConnect4(str0, str1, str2, str3, str4)      @""str0""str1""str2""str3""str4""
-#define NN_stringConnect5(str0, str1, str2, str3, str4, str5)      @""str0""str1""str2""str3""str4""str5""
-#define NN_stringConnect6(str0, str1, str2, str3, str4, str5, str6)      @""str0""str1""str2""str3""str4""str5""str6""
-#define NN_stringConnect7(str0, str1, str2, str3, str4, str5, str6, str7)      @""str0""str1""str2""str3""str4""str5""str6""str7""
-#define NN_stringConnect8(str0, str1, str2, str3, str4, str5, str6, str7, str8)      @""str0""str1""str2""str3""str4""str5""str6""str7""str8""
-#define NN_stringConnect9(str0, str1, str2, str3, str4, str5, str6, str7, str8, str9)      @""str0""str1""str2""str3""str4""str5""str6""str7""str8""str9""
+#define NN_stringConnect1(str0, str1)      @"" str0 "" str1 ""
+#define NN_stringConnect2(str0, str1, str2)      @"" str0 "" str1 "" str2 ""
+#define NN_stringConnect3(str0, str1, str2, str3)      @"" str0 "" str1 "" str2 "" str3 ""
+#define NN_stringConnect4(str0, str1, str2, str3, str4)      @"" str0 "" str1 "" str2 "" str3 "" str4 ""
+#define NN_stringConnect5(str0, str1, str2, str3, str4, str5)      @"" str0 "" str1 "" str2 "" str3 "" str4 "" str5 ""
+#define NN_stringConnect6(str0, str1, str2, str3, str4, str5, str6)      @"" str0 "" str1 "" str2 "" str3 "" str4 "" str5 "" str6 ""
+#define NN_stringConnect7(str0, str1, str2, str3, str4, str5, str6, str7)      @"" str0 "" str1 "" str2 "" str3 "" str4 "" str5 "" str6 "" str7 ""
+#define NN_stringConnect8(str0, str1, str2, str3, str4, str5, str6, str7, str8)      @"" str0 "" str1 "" str2 "" str3 "" str4 "" str5 "" str6 "" str7 "" str8 ""
+#define NN_stringConnect9(str0, str1, str2, str3, str4, str5, str6, str7, str8, str9)      @"" str0 "" str1 "" str2 "" str3 "" str4 "" str5 "" str6 "" str7 "" str8 "" str9 ""
 
 #endif /* NNMacrosString_h */
