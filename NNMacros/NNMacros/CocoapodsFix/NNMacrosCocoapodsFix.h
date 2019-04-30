eb02549152ea72fb2aa1cefcb462d6f9924791e8
GuHaijun <243297288@qq.com>
2019-04-30 11:38:29 +0800

    #fix Include of non-modular header inside framework module

---
+++ NNMacros/NNMacros/CocoapodsFix/NNMacrosCocoapodsFix.h
@@ -0,0 +1,19 @@
+//
+//  NNMacrosCocoapodsFix.h
+//  NNMacros
+//
+//  Created by GuHaijun on 2019/4/30.
+//  Copyright © 2019 GuHaijun. All rights reserved.
+//
+
+//  Just Fix: "Include of non-modular header inside framework module"
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NNMacrosCocoapodsFix : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NNMacros/NNMacros/CocoapodsFix/NNMacrosCocoapodsFix.m
@@ -0,0 +1,13 @@
+//
+//  NNMacrosCocoapodsFix.m
+//  NNMacros
+//
+//  Created by GuHaijun on 2019/4/30.
+//  Copyright © 2019 GuHaijun. All rights reserved.
+//
+
+#import "NNMacrosCocoapodsFix.h"
+
+@implementation NNMacrosCocoapodsFix
+
+@end
