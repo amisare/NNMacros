ac714a52355d69410dc2fa385bf69d0c3d3d45ff
顾海军 <guhaijun3@jd.com>
2019-09-27 14:31:16 +0800

    add NN_screen 中增加 3.5inch 和 3.5inch 1x 定义 add 增加 NN_screenSize 定义

--- NNMacros/NNMacros/NNMacrosScreen.h
+++ NNMacros/NNMacros/NNMacrosScreen.h
@@ -31,12 +31,47 @@
     isZoomed;\
 }\
 
-#define NN_screenIs3Dot5inch   CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs4inch       CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs4Dot7inch   CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs5Dot5inch   CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs5Dot8inch   CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs6Dot1inch   CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size)
-#define NN_screenIs6Dot5inch   CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size)
+// iPhone
+// iPhone 2G
+// iPhone 3
+// iPhone 3GS
+#define NN_screenSize3Dot5inch1x    CGSizeMake(320, 480)
+// iPhone 4
+// iPhone 4s
+#define NN_screenSize3Dot5inch      CGSizeMake(640, 960)
+// iPhone 5
+// iPhone 5s
+// iPhone 5c
+// iPhone SE
+#define NN_screenSize4inch          CGSizeMake(640, 1136)
+// iPhone 6
+// iPhone 6s
+// iPhone 7
+// iPhone 8
+#define NN_screenSize4Dot7inch      CGSizeMake(750, 1334)
+// iPhone 6
+// iPhone 6s
+// iPhone 7
+// iPhone 8
+#define NN_screenSize5Dot5inch      CGSizeMake(1242, 2208)
+// iPhone X
+// iPhone XS
+// iPhone 11 Pro
+#define NN_screenSize5Dot8inch      CGSizeMake(1125, 2436)
+// iPhone XR
+// iPhone 11
+#define NN_screenSize6Dot1inch      CGSizeMake(828, 1792)
+// iPhone XR
+// iPhone 11
+#define NN_screenSize6Dot5inch      CGSizeMake(1242, 2688)
+
+#define NN_screenIs3Dot5inch1x      CGSizeEqualToSize(NN_screenSize3Dot5inch1x, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs3Dot5inch        CGSizeEqualToSize(NN_screenSize3Dot5inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs4inch            CGSizeEqualToSize(NN_screenSize4inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs4Dot7inch        CGSizeEqualToSize(NN_screenSize4Dot7inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs5Dot5inch        CGSizeEqualToSize(NN_screenSize5Dot5inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs5Dot8inch        CGSizeEqualToSize(NN_screenSize5Dot8inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs6Dot1inch        CGSizeEqualToSize(NN_screenSize6Dot1inch, [[UIScreen mainScreen] currentMode].size)
+#define NN_screenIs6Dot5inch        CGSizeEqualToSize(NN_screenSize6Dot5inch, [[UIScreen mainScreen] currentMode].size)
 
 #endif /* NNMacrosScreen_h */
