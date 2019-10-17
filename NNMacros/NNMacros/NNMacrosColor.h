c65c0f37b822fa658749b088fdd8ba1e16990083
顾海军 <guhaijun3@jd.com>
2019-10-17 10:41:24 +0800

    代码格式化

--- NNMacros/NNMacros/NNMacrosColor.h
+++ NNMacros/NNMacros/NNMacrosColor.h
@@ -9,11 +9,11 @@
 #ifndef NNMacrosColor_h
 #define NNMacrosColor_h
 
-#define NN_colorWithRGBAlpha(r, g, b, a)       [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a/1.0]
-#define NN_colorWithRGB(r, g, b)               NN_colorWithRGBAlpha(r, g, b, 1.0)
-#define NN_colorWithHexAlpha(h, a)             [UIColor colorWithRed:((h>>16)&0xff)/255.0 green:((h>>8)&0xff)/255.0 blue:(h&0xff)/255.0 alpha:a/1.0]
-#define NN_colorWithHex(h)                     NN_colorWithHexAlpha(h, 1.0)
+#define NN_colorWithRGBAlpha(r, g, b, a)        [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a/1.0]
+#define NN_colorWithRGB(r, g, b)                NN_colorWithRGBAlpha(r, g, b, 1.0)
+#define NN_colorWithHexAlpha(h, a)              [UIColor colorWithRed:((h>>16)&0xff)/255.0 green:((h>>8)&0xff)/255.0 blue:(h&0xff)/255.0 alpha:a/1.0]
+#define NN_colorWithHex(h)                      NN_colorWithHexAlpha(h, 1.0)
 // Rand Color
-#define NN_colorRand                           [UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:1.0]
+#define NN_colorRand                            [UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:1.0]
 
 #endif /* NNMacrosColor_h */
